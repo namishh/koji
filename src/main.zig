f291f1ebf2266b1c588708bcbb5cb0d13f120978
namish <namish@tuta.io>
2025-06-08 00:52:01 +0530

    save

---
+++ src/main.zig
@@ -0,0 +1,19 @@
+const rl = @import("raylib");
+
+pub fn main() anyerror!void {
+    const screenWidth = 800;
+    const screenHeight = 450;
+
+    rl.initWindow(screenWidth, screenHeight, "koji");
+    defer rl.closeWindow();
+
+    rl.setTargetFPS(60);
+    while (!rl.windowShouldClose()) {
+        rl.beginDrawing();
+        defer rl.endDrawing();
+
+        rl.clearBackground(.white);
+
+        rl.drawText("Congrats! You created your first window!", 190, 200, 20, .light_gray);
+    }
+}
